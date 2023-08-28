8c07e2be6e534c83f9bf7429f5fd5a37525fcdaa
Nastassia Stanchyk <stanchikaf@gmail.com>
2023-08-28 15:33:32 +0300

    Add iOS locators

--- src/main/java/hw3/pageObjects/NativePageObject.java
+++ src/main/java/hw3/pageObjects/NativePageObject.java
@@ -3,34 +3,52 @@ package hw3.pageObjects;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class NativePageObject {
 
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
+            @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@name=\"Sign In\"]")
     WebElement signInBtn;
     @AndroidFindBy(id = "android:id/button1")
+//    @iOSXCUITFindBy(xpath = "")
     WebElement okBtn;
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@name=\"Register new account\"]")
     WebElement registerBtn;
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_email")
+    // TODO: пересмотреть локатор
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeApplication[@name=\"EPAMTestApp\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[1]/XCUIElementTypeTextField")
     WebElement regEmail;
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_username")
+    // TODO: пересмотреть локатор
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeApplication[@name=\"EPAMTestApp\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[2]/XCUIElementTypeTextField")
     WebElement regUsername;
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_password")
+    // TODO: пересмотреть локатор
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeApplication[@name=\"EPAMTestApp\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[3]/XCUIElementTypeSecureTextField")
     WebElement regPassword;
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_confirm_password")
+    // TODO: пересмотреть локатор
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeApplication[@name=\"EPAMTestApp\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[4]/XCUIElementTypeSecureTextField")
     WebElement regConfirmPassword;
     @AndroidFindBy(className = "android.widget.CheckedTextView")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeSwitch[@name=\"I read agreaments and agree wit it\"]")
     WebElement confirmCheckbox;
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_new_account_button")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@name=\"Register new account\"]")
     WebElement registerNewAccountBtn;
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_email")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeApplication[@name=\"EPAMTestApp\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[1]/XCUIElementTypeTextField")
     WebElement loginEmail;
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_pwd")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeApplication[@name=\"EPAMTestApp\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[2]/XCUIElementTypeSecureTextField")
     WebElement loginPwd;
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/action_bar")
+    // TODO: пересмотреть локатор
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeNavigationBar[@name=\"Budget\"]")
     WebElement actionBar;
 
     public static final String TEXT_ON_PAGE = "android.widget.TextView";
--- src/main/java/hw3/setup/BaseTest.java
+++ src/main/java/hw3/setup/BaseTest.java
@@ -3,6 +3,7 @@ package hw3.setup;
 import hw3.pageObjects.PageObject;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.MutableCapabilities;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
@@ -24,42 +25,40 @@ public class BaseTest implements IDriver {
         return po;
     }
 
-    @Parameters({"automationName", "platformName","appType","deviceName","browserName","app"})
+    @Parameters({"deviceOrientation", "platformVersion", "appiumVersion",
+            "automationName", "platformName","appType","deviceName","browserName","app"})
     @BeforeSuite(alwaysRun = true)
-    public void setUp(String automationName, String platformName, String appType, String deviceName,
+    public void setUp(String deviceOrientation, String platformVersion, String appiumVersion, String automationName,
+                      String platformName, String appType, String deviceName,
                       @Optional("") String browserName,
                       @Optional("") String app) throws Exception {
         System.out.println("Before: app type - "+appType);
-        setAppiumDriver(automationName, platformName, deviceName, browserName, app);
+        setAppiumDriver(deviceOrientation, platformVersion, appiumVersion, automationName, platformName, deviceName,
+                browserName, app);
         setPageObject(appType, appiumDriver);
 
     }
 
     @AfterSuite(alwaysRun = true)
-    public void tearDown() throws Exception {
+    public void tearDown(ITestResult result) {
         System.out.println("After");
         appiumDriver.closeApp();
+        appiumDriver.quit();
     }
 
-    private void setAppiumDriver(String automationName, String platformName, String deviceName, String browserName,
-                                 String app){
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-        //mandatory Android capabilities
-//        capabilities.setCapability("automationName",automationName);
-//        capabilities.setCapability("platformName",platformName);
-//        capabilities.setCapability("deviceName",deviceName);
-
+    private void setAppiumDriver(String deviceOrientation, String platformVersion, String appiumVersion, String automationName,
+                                 String platformName, String deviceName, String browserName, String app) {
         MutableCapabilities caps = new MutableCapabilities();
         caps.setCapability("platformName", platformName);
-        if(app.endsWith(".apk")) {
+        if(app.endsWith(".apk") || app.endsWith(".ipa")) {
             caps.setCapability("appium:app", "storage:filename=" + app);  // The filename of the mobile app
         }
         caps.setCapability("appium:deviceName", deviceName);
-        caps.setCapability("appium:deviceOrientation", "portrait");
-        caps.setCapability("appium:platformVersion", "13.0");
+        caps.setCapability("appium:deviceOrientation", deviceOrientation);
+        caps.setCapability("appium:platformVersion", platformVersion);
         caps.setCapability("appium:automationName", automationName);
         MutableCapabilities sauceOptions = new MutableCapabilities();
-        sauceOptions.setCapability("appiumVersion", "1.22.3");
+        sauceOptions.setCapability("appiumVersion", appiumVersion);
         sauceOptions.setCapability("username", System.getProperty("username"));
         sauceOptions.setCapability("accessKey", System.getProperty("accessKey"));
         sauceOptions.setCapability("build", "appium-build-2LITO");
@@ -70,7 +69,6 @@ public class BaseTest implements IDriver {
         caps.setCapability("chromedriverDisableBuildCheck","true");
 
         try {
-//            appiumDriver = new AppiumDriver(new URL(System.getProperty("ts.appium")), capabilities);
             URL url = new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
             appiumDriver = new AppiumDriver(url, caps);
         } catch (MalformedURLException e) {
